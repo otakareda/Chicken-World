84266998f8577171a0c0af62d938f6454b9218db
Otaka <otakareda784@gmail.com>
2024-06-03 18:59:26 -0700

    updates

---
+++ Javascript.js
@@ -0,0 +1,371 @@
+function d1() {
+  alert("Day 1");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;1";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;42";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 0";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 35";
+  // End every-day
+}
+
+function d2() {
+  alert("DAY 2");
+  // Start every-day
+
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;2";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;50";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 0";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 34";
+  // End every-day
+}
+function d3() {
+  alert("Day 3");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;3";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;74";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 0";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 34";
+  // End every-day
+}
+
+function d4() {
+  alert("DAY 4");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;4";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;90";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 3";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 34";
+  // End every-day
+}
+function d5() {
+  alert("Day 5");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;5";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;109";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 3";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 33";
+  // End every-day
+}
+function d6() {
+  alert("Day 6");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;6";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;42";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 0";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 35";
+  // End every-day
+}
+
+function d2a() {
+  alert("DAY 2");
+  // Start every-day
+
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;2";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;50";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 0";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 34";
+  // End every-day
+}
+function d3() {
+  alert("Day 3");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;3";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;74";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 0";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 34";
+  // End every-day
+}
+
+function d4() {
+  alert("DAY 4");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;4";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;90";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 3";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 34";
+  // End every-day
+}
+function d5() {
+  alert("Day 5");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;5";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;109";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 3";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 33";
+  // End every-day
+}
+function d1() {
+  alert("Day 1");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;1";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;42";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 0";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 35";
+  // End every-day
+}
+
+function d2() {
+  alert("DAY 2");
+  // Start every-day
+
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;2";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;50";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 0";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 34";
+  // End every-day
+}
+function d3() {
+  alert("Day 3");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;3";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;74";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 0";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 34";
+  // End every-day
+}
+
+function d4() {
+  alert("DAY 4");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;4";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;90";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 3";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 34";
+  // End every-day
+}
+function d5() {
+  alert("Day 5");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;5";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;109";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 3";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 33";
+  // End every-day
+}
+function d1() {
+  alert("Day 1");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;1";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;42";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 0";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 35";
+  // End every-day
+}
+
+function d2() {
+  alert("DAY 2");
+  // Start every-day
+
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;2";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;50";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 0";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 34";
+  // End every-day
+}
+function d3() {
+  alert("Day 3");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;3";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;74";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 0";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 34";
+  // End every-day
+}
+
+function d4() {
+  alert("DAY 4");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;4";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;90";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 3";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 34";
+  // End every-day
+}
+function d5() {
+  alert("Day 5");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;5";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;109";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 3";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 33";
+  // End every-day
+}
+function d1() {
+  alert("Day 1");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;1";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;42";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 0";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 35";
+  // End every-day
+}
+
+function d2() {
+  alert("DAY 2");
+  // Start every-day
+
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;2";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;50";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 0";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 34";
+  // End every-day
+}
+function d3() {
+  alert("Day 3");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;3";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;74";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 0";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 34";
+  // End every-day
+}
+
+function d4() {
+  alert("DAY 4");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;4";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;90";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 3";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 34";
+  // End every-day
+}
+function d5() {
+  alert("Day 5");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;5";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;109";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 3";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 33";
+  // End every-day
+}
+function d1() {
+  alert("Day 1");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;1";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;42";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 0";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 35";
+  // End every-day
+}
+
+function d2() {
+  alert("DAY 2");
+  // Start every-day
+
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;2";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;50";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 0";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 34";
+  // End every-day
+}
+function d3() {
+  alert("Day 3");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;3";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;74";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 0";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 34";
+  // End every-day
+}
+
+function d4() {
+  alert("DAY 4");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;4";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;90";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 3";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 34";
+  // End every-day
+}
+function d5() {
+  alert("Day 5");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;5";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;109";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 3";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 33";
+  // End every-day
+}
+function d1() {
+  alert("Day 1");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;1";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;42";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 0";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 35";
+  // End every-day
+}
+
+function d2() {
+  alert("DAY 2");
+  // Start every-day
+
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;2";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;50";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 0";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 34";
+  // End every-day
+}
+function d3() {
+  alert("Day 3");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;3";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;74";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 0";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 34";
+  // End every-day
+}
+
+function d4() {
+  alert("DAY 4");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;4";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;90";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 3";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 34";
+  // End every-day
+}
+function d5() {
+  alert("Day 5");
+  // Start every-day
+  document.getElementById("days").innerHTML = "اليوم &nbsp;&nbsp;5";
+  document.getElementById("wights").innerHTML = "الوزن &nbsp;&nbsp;109";
+  document.getElementById("lights").innerHTML = "ساعات الاظلام&nbsp;&nbsp; 3";
+  document.getElementById("temperatures").innerHTML =
+    "درجة الحرارة&nbsp;&nbsp; 33";
+  // End every-day
+}
